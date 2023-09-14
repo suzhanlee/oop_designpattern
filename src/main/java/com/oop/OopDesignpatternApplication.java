ca9b4400b74f4b91b9e47f78a196fa097a1333f2
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-09-14 18:43:48 +0900

    first commit

---
+++ src/main/java/com/oop/OopDesignpatternApplication.java
@@ -0,0 +1,13 @@
+package com.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopDesignpatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopDesignpatternApplication.class, args);
+	}
+
+}
